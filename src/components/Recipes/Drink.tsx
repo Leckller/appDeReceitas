383d7e52d5a7e1a796fd9d4680a296631d980238
Leckller <7leckler@gmail.com>
2023-11-28 13:05:05 -0300

    feat: req 22

--- src/components/Recipes/Drink.tsx
+++ src/components/Recipes/Drink.tsx
@@ -1,25 +1,30 @@
+import { Link } from 'react-router-dom';
 import { Drinks } from '../../types';
 
 function Drink({ drinks }: { drinks: Drinks[] }) {
   return (
     <section className="flex w-screen flex-wrap gap-4 p-2">
       {drinks && drinks.slice(0, 12).map((d, i) => (
-        <article
-          className="w-64 h-64 flex items-center flex-col
-            justify-around border border-gray-900 rounded-md"
-          data-testid={ `${i}-recipe-card` }
+        <Link
+          to={ `/drinks/${d.idDrink}` }
           key={ d.idDrink }
         >
-          <img
-            className="w-1/2"
-            src={ d.strDrinkThumb }
-            alt={ d.strDrink }
-            data-testid={ `${i}-card-img` }
-          />
-          <h1 data-testid={ `${i}-card-name` }>
-            {d.strDrink}
-          </h1>
-        </article>
+          <article
+            className="w-64 h-64 flex items-center flex-col
+            justify-around border border-gray-900 rounded-md"
+            data-testid={ `${i}-recipe-card` }
+          >
+            <img
+              className="w-1/2"
+              src={ d.strDrinkThumb }
+              alt={ d.strDrink }
+              data-testid={ `${i}-card-img` }
+            />
+            <h1 data-testid={ `${i}-card-name` }>
+              {d.strDrink}
+            </h1>
+          </article>
+        </Link>
       ))}
     </section>
   );
--- src/components/Recipes/Meal.tsx
+++ src/components/Recipes/Meal.tsx
@@ -1,26 +1,32 @@
+import { Link } from 'react-router-dom';
 import { Meals } from '../../types';
 
 function Meal({ meals }: { meals: Meals[] }) {
   return (
     <section className="flex w-screen flex-wrap gap-4 p-2">
       {meals && meals.slice(0, 12).map((d, i) => (
-        <article
-          className="w-64 h-64 flex items-center flex-col
+        <Link
+          to={ `/meals/${d.idMeal}` }
+          key={ d.idMeal }
+
+        >
+          <article
+            className="w-64 h-64 flex items-center flex-col
             justify-around
           bg-red-400"
-          data-testid={ `${i}-recipe-card` }
-          key={ d.idMeal }
-        >
-          <img
-            className="w-1/2"
-            src={ d.strMealThumb }
-            alt={ d.strMeal }
-            data-testid={ `${i}-card-img` }
-          />
-          <h1 data-testid={ `${i}-card-name` }>
-            {d.strMeal}
-          </h1>
-        </article>
+            data-testid={ `${i}-recipe-card` }
+          >
+            <img
+              className="w-1/2"
+              src={ d.strMealThumb }
+              alt={ d.strMeal }
+              data-testid={ `${i}-card-img` }
+            />
+            <h1 data-testid={ `${i}-card-name` }>
+              {d.strMeal}
+            </h1>
+          </article>
+        </Link>
       ))}
     </section>
   );
