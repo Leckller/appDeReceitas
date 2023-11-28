1435b6bb88bc0b3e753d40b337b25113e6afa0be
JohnathanSpinola <122413268+JohnathanSpinola@users.noreply.github.com>
2023-11-28 13:27:47 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './redux/store';
 
-
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
