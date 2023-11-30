32cbf4108b8304fedd70ba531863e206956b1661
Leckller <7leckler@gmail.com>
2023-11-29 23:01:52 -0300

    feat: footer provisorio

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import mealIcon from '../../images/mealIcon.svg';
 import drinkIcon from '../../images/drinkIcon.svg';
-import './Footer.css';
 
 function Footer() {
   const navigate = useNavigate();
@@ -18,29 +16,26 @@ function Footer() {
   return (
     <footer
       data-testid="footer"
-      style={ { position: 'fixed',
-        bottom: 0,
-        width: '100%',
-        backgroundColor: '#f2f2f2' } }
+      className="fixed w-screen bottom-0 bg-transparent
+      flex flex-row justify-center items-center gap-5
+      backdrop-blur-sm h-9"
     >
-      <div className="footer-container">
-        <input
-          onClick={ handleNavigateToMeals }
-          data-testid="meals-bottom-btn"
-          type="image"
-          className="footer-icon"
-          src={ mealIcon }
-          alt="Comidas"
-        />
-        <input
-          onClick={ handleNavigateToDrinks }
-          data-testid="drinks-bottom-btn"
-          type="image"
-          className="footer-icon"
-          src={ drinkIcon }
-          alt="Bebidas"
-        />
-      </div>
+      <input
+        onClick={ handleNavigateToMeals }
+        data-testid="meals-bottom-btn"
+        type="image"
+        className="footer-icon"
+        src={ mealIcon }
+        alt="Comidas"
+      />
+      <input
+        onClick={ handleNavigateToDrinks }
+        data-testid="drinks-bottom-btn"
+        type="image"
+        className=""
+        src={ drinkIcon }
+        alt="Bebidas"
+      />
     </footer>
   );
 }
