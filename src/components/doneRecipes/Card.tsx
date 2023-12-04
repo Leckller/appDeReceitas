5ab0a3e7f82096864994ee01101c58617d23a4cb
Leckller <7leckler@gmail.com>
2023-12-04 11:56:40 -0300

    feat: card and doneRecipes page

---
+++ src/components/doneRecipes/Card.tsx
@@ -0,0 +1,46 @@
+import { Favorite } from '../../types';
+
+function Card({ index, recipe }: { index: number, recipe:Favorite }) {
+  return (
+    <div className="flex flex-row w-full border border-gray-400 rounded-xl">
+      <div className="w-1/2">
+        <img
+          src={ recipe.image }
+          alt={ recipe.name }
+          data-testid={ `${index}-horizontal-image` }
+        />
+      </div>
+      <div className="w-1/2 p-2">
+
+        <div className="w-full flex flex-row flex-wrap">
+          <div className="flex flex-row w-full justify-between">
+            <h2 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h2>
+            <button data-testid={ `${index}-horizontal-share-btn` }>share</button>
+          </div>
+          <h3 data-testid={ `${index}-horizontal-top-text` }>{recipe.category}</h3>
+        </div>
+
+        <div>
+          <h4 data-testid={ `${index}-horizontal-done-date` }>
+            {recipe.doneDate}
+          </h4>
+
+          <div className="flex flex-row gap-5">
+            {recipe.tags && recipe.tags.map((tag) => (
+              <h5
+                key={ tag }
+                data-testid={ `${index}-${recipe.tags}-horizontal-tag` }
+              >
+                {tag}
+              </h5>
+            ))}
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/DoneRecipes.tsx
+++ src/pages/DoneRecipes.tsx
@@ -1,6 +1,45 @@
+import { useEffect, useState } from 'react';
+import { getItem } from '../utils/localStorage';
+import Card from '../components/doneRecipes/Card';
+import { Favorite } from '../types';
+
 function DoneRecipes() {
+  const [doneRec, setDoneRec] = useState<Favorite[]>([]);
+  useEffect(() => {
+    setDoneRec(getItem('doneRecipes'));
+  }, []);
+  console.log(doneRec);
   return (
-    <div>Done Recipes</div>
+    <div className="w-screen h-screen flex flex-col items-center pt-5 gap-5">
+      <div className="w-screen flex flex-row justify-center gap-10">
+        <button
+          className="rounded-full border border-black w-20 h-20"
+          data-testid="filter-by-all-btn"
+        >
+          All
+
+        </button>
+        <button
+          className="rounded-full border border-black w-20 h-20"
+          data-testid="filter-by-meal-btn"
+        >
+          Meals
+
+        </button>
+        <button
+          className="rounded-full border border-black w-20 h-20"
+          data-testid="filter-by-drink-btn"
+        >
+          Drinks
+
+        </button>
+      </div>
+      <div className="flex flex-col w-full p-5">
+        {doneRec && doneRec.map((recipe, index) => (
+          <Card index={ index } recipe={ recipe } key={ recipe.id } />
+        ))}
+      </div>
+    </div>
   );
 }
 
