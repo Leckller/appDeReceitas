1973ab852cf0c9d4f0380c4bc9e3755f2cb23615
Joao Vitor <jvitorss2021@gmail.com>
2023-12-04 16:09:28 -0300

    att

--- src/tests/Footer.test.tsx
+++ src/tests/Footer.test.tsx
@@ -1,30 +1,28 @@
 import { screen } from '@testing-library/dom';
-import userEvent from '@testing-library/user-event';
-import renderWithRouter from './helpers/renderWithRouter';
-import Layout from '../components/Header/Layout';
+import renderWithRouterAndRedux from './helpers/renderWithRedux';
+import App from '../App';
 
 describe('Verificando o componente footer', () => {
-  test('Verificando se o Footer renderiza corretamente', () => {
-    renderWithRouter(<Layout />);
-
-    const footer = screen.getByTestId('footer');
-    expect(footer).toBeInTheDocument();
-  });
-  test('Verifica se o botão redireciona à página de bebidas', async () => {
-    renderWithRouter(<Layout />);
-
-    const drinksBtn = screen.getByTestId('drinks-bottom-btn');
-
-    await userEvent.click(drinksBtn);
-    expect(window.location.pathname).toBe('/drinks');
-  });
-
-  test('verifica se o botão redireciona à página de comidas', async () => {
-    renderWithRouter(<Layout />);
+  test('Verificando se o Footer renderiza corretamente', async () => {
+    const { user } = renderWithRouterAndRedux(<App />, '/meals');
 
     const mealsBtn = screen.getByTestId('meals-bottom-btn');
+    const drinksBtn = screen.getByTestId('drinks-bottom-btn');
 
-    await userEvent.click(mealsBtn);
-    expect(window.location.pathname).toBe('/meals');
+    await user.click(drinksBtn);
+
+    let loading = screen.getByRole('heading', { level: 1, name: 'Loading ...' });
+
+    expect(loading).toBeVisible();
+
+    const article = await screen.findAllByRole('article');
+
+    expect(article).toHaveLength(12);
+
+    await user.click(mealsBtn);
+
+    loading = screen.getByRole('heading', { level: 1, name: 'Loading ...' });
+
+    expect(loading).toBeVisible();
   });
 });
