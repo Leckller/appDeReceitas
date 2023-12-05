20e470b4962002d7a3d1bf1094635b181efd0197
JohnathanSaboia <nftjohnsls@gmail.com>
2023-12-05 11:03:34 -0300

    fix: lint test Profile

--- src/tests/Profile.test.tsx
+++ src/tests/Profile.test.tsx
@@ -2,14 +2,13 @@ import { screen, fireEvent } from '@testing-library/dom';
 import renderWithRouterAndRedux from './helpers/renderWithRedux';
 import App from '../App';
 
-// describe('Check page Profile', () => {
-//   // test('Checks profile functionality', async () => {
-//   //   const { user } = renderWithRouterAndRedux(<App />, '/profile');
-//   //   screen.getByRole('heading', { level: 1, name: 'Profile' });
+describe('Check page Profile', () => {
+  test('Checks profile functionality', async () => {
+    const { user } = renderWithRouterAndRedux(<App />, '/profile');
+    //   screen.getByRole('heading', { level: 1, name: 'Profile' });
 
-//   //   await user.click(screen.getByTestId('profile-top-btn'));
-//   // });
-// });
+    //   await user.click(screen.getByTestId('profile-top-btn'));
+    // });
     const userEmailElement = screen.getByTestId('profile-email');
     expect(userEmailElement).toBeInTheDocument();
 
@@ -33,18 +32,17 @@ import App from '../App';
     expect(window.location.pathname).toBe('/done-recipes');
   });
   test('verifica se após o click a pagina é redirecionada para receitas favoritas', async () => {
-    const { user } = renderWithRouterAndRedux(<App />, '/profile');
+    renderWithRouterAndRedux(<App />, '/profile');
 
     const favoriteRecipesBtn = screen.getByTestId('profile-favorite-btn');
     fireEvent.click(favoriteRecipesBtn);
     expect(window.location.pathname).toBe('/favorite-recipes');
   });
   test('verifica se após o click a pagina é redirecionada para tela de login', async () => {
-    const { user } = renderWithRouterAndRedux(<App />, '/profile');
+    renderWithRouterAndRedux(<App />, '/profile');
 
     const logoutBtn = screen.getByTestId('profile-logout-btn');
     fireEvent.click(logoutBtn);
     expect(window.location.pathname).toBe('/');
   });
 });
-
